75087ae0e0be32b226ee066251bdbd5e5d8c8905
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-03-17 18:06:45 +0000

    Added scripts

---
+++ Project Paradox/Assets/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TMP_Text NameText;
+    public TMP_Text MainText;
+
+
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        NameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        //MainText.text = sentence;
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        MainText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            MainText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+
+    }
+}
---
+++ Project Paradox/Assets/DialogueTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    [TextArea(3, 10)]
+    public List<string> StartDialogue;
+    [TextArea(3, 10)]
+    public List<string> TransitionalDialogue;
+    [TextArea(3, 10)]
+    public List<string> EndDialogue;
+
+    public void Awake()
+    {
+       
+    }
+
+    public void TriggerDialogue()
+    {
+        dialogue.sentences[0] = StartDialogue[Random.Range(0, StartDialogue.Count)];
+        dialogue.sentences[1] = TransitionalDialogue[Random.Range(0, StartDialogue.Count)];
+        dialogue.sentences[2] = EndDialogue[Random.Range(0, StartDialogue.Count)];
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
